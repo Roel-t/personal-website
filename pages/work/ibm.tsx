77909134c8c001e6d7b7f8120371fed0938f361f
Roel Tijerina <tijerinaroel@gmail.com>
2022-05-06 18:33:12 -0500

    add work pages

---
+++ pages/work/ibm.tsx
@@ -0,0 +1,22 @@
+import { Container, Grid, Typography} from '@mui/material';
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Ibm: NextPage = () => {
+    return (
+        <Container>
+        <Head>
+          <title>Roel Tijerina | IBM</title>
+        </Head>
+        <Grid container className="grid-row-1" alignItems={"center"}>
+            <Grid item xs={12} md={12} lg={12} sx={{textAlign:"center"}}>     
+                <Typography variant="h6" color="red" sx={{padding:".5rem .5rem"}}>
+                  🚧 Currently under construction, Sorry! 🚧
+                </Typography>
+            </Grid>
+        </Grid>
+        
+      </Container>);
+}
+ 
+export default Ibm;
---
+++ pages/work/mth.tsx
@@ -0,0 +1,22 @@
+import { Container, Grid, Typography} from '@mui/material';
+import type { NextPage } from 'next';
+import Head from 'next/head';
+
+const Mth: NextPage = () => {
+    return (
+        <Container>
+        <Head>
+          <title>Roel Tijerina | MTH</title>
+        </Head>
+        <Grid container className="grid-row-1" alignItems={"center"}>
+            <Grid item xs={12} md={12} lg={12} sx={{textAlign:"center"}}>     
+                <Typography variant="h6" color="red" sx={{padding:".5rem .5rem"}}>
+                  🚧 Currently under construction, Sorry! 🚧
+                </Typography>
+            </Grid>
+        </Grid>
+        
+      </Container>);
+}
+ 
+export default Mth;

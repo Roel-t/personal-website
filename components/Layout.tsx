98887f204a5d5667ff4cb70e7349474e1cabe7eb
Roel Tijerina <tijerinaroel@gmail.com>
2022-05-06 20:37:46 -0500

    updated meta tags

--- components/Layout.tsx
+++ components/Layout.tsx
@@ -18,23 +18,26 @@ type Props = {children?:any}
 
 const Layout = ({children}:Props) => {
     return (  
-        <ThemeProvider theme={theme}>
-            <Head>
-                <meta name="author" content="Roel Tijerina"/>
-                <meta name="description" content="Roel Tijerina's personal website. Software engineer based from Texas working as a full stack engineer at IBM. Currently using React, NodeJs, and expressJs." />
-                <link rel="canonical" href="https://www.roeltijerina.com/"/>
-                <link rel="icon" href="/favicon.ico" />
-                <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet"/> 
-                <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
-                <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
-            </Head>
-            <div className="content">
-                <Navbar/>
-                {children}
-                <Footer/>
-            </div>
-        </ThemeProvider>
-        
+            <ThemeProvider theme={theme}>
+                <Head>
+                    <meta charSet="utf-8"/>
+                    <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
+                    <link rel="canonical" href="https://www.roeltijerina.com/"/>
+                    {/* Google */}
+                    <meta name="author" content="Roel Tijerina"/>
+                    <meta name="description" content="Roel Tijerina's personal website. Software engineer based from Texas working as a full stack engineer at IBM. Currently using React, NodeJs, and expressJs." />
+
+                    <link rel="icon" href="/favicon.ico" />
+                    <link href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,300;0,400;0,500;0,700;1,300;1,400;1,500;1,700&display=swap" rel="stylesheet"/> 
+                    <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Roboto:300,400,500,700&display=swap"/>
+                    <link rel="stylesheet" href="https://fonts.googleapis.com/icon?family=Material+Icons"/>
+                </Head>
+                <div className="content">
+                    <Navbar/>
+                    {children}
+                    <Footer/>
+                </div>
+            </ThemeProvider>   
     );
 }
  
